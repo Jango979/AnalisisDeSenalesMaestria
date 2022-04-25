a6f904442878dbdf60e15a15de1cd38fea38187e
Jango979 <bravoac2016@licifug.ugto.mx>
2022-04-25 00:02:28 -0500

    Functions

--- Compiler.py
+++ Compiler.py
@@ -2,9 +2,12 @@ from Directory import Directory
 from FileTreatment import File
 A = Directory()
 #A.excludeByFormat(exclude=False)
-A.excludeByFiles(["Hola.png"])
+A.excludeByFiles(["Hola.png","ChuyRelajacion.xlsx","Concentracion chuy.xlsx"])
 A.excludeByFormat(".acq")
 A.getFileList()
 
+
 for i in A.FilesLocation:
     B= File(i)
+    B.AddTime(noSample=1000,tf=180)
+    print(B)
--- FileTreatment.py
+++ FileTreatment.py
@@ -1,15 +1,21 @@
-import pandas as pd
-
+from pandas import read_excel,read_csv
+from numpy import arange
 class File():
-    def __init__(self,chdir):
+    def __init__(self,chdir,names=["Impedance","Phase","Module"],skip=0):
         self.chdir = chdir
-        self.File= self.chdir.split("\\")[-1]
-        print(self.File)
-        """
+        self.file= self.chdir.split("\\")[-1]
+        print("Leyendo archivo {}".format(self.file))
+
         if chdir.endswith(".csv"):
-            self.File=
+            self.formatFile = "csv"
+            self.DataFrame = read_csv(chdir, names=names, skiprows=skip)
         elif chdir.endswith(".xlsx"):
-            pass
-        """
-    def AddTime(self,NoSample=1000,T=3):
-        pass
+            self.formatFile ="excel"
+            self.DataFrame = read_excel(chdir, names=names, skiprows=skip)
+        self.DataFrame = self.DataFrame.drop(len(self.DataFrame[:]) - 1)
+
+    def AddTime(self,noSample=1000,to=0,tf=3):
+        vTime = arange(to, tf-1/noSample, 1/noSample)
+        self.DataFrame.insert(0,"Time",vTime)
+
+
